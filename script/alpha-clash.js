19af6966e6ff3e2580a871a172f0418c50ccf7c6
UAShoshi <ummeaymonafifha@gmail.com>
2024-02-15 17:56:36 +0600

    set the background color

--- script/alpha-clash.js
+++ script/alpha-clash.js
@@ -7,4 +7,38 @@ function play() {
   const playGroundSection = document.getElementById('play-ground');
   playGroundSection.classList.remove('hidden');
   // console.log('play Now pro');
+  continueGame();
+}
+
+function continueGame() {
+  const alphabet = getRendomAlphabet();
+  console.log(alphabet); 
+
+  // current-alphabet Screen
+  const currentAlphabetElement = document.getElementById('current-alphabet');
+  currentAlphabetElement.innerText = alphabet;
+
+// set back ground color
+  setBackgroundColorById(alphabet)
+}
+
+// get or create an alphabet arry
+function getRendomAlphabet() {
+  const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabets = alphabetString.split('');
+  // console.log(alphabets);
+
+  // get a random index between 0-25
+  const randomNumber = Math.random() * 25;
+  const index = Math.round(randomNumber);
+  
+  const alphabet = alphabets[index];
+  // console.log(alphabet);
+  return alphabet;
+}
+
+
+function setBackgroundColorById(elementId) {
+  const element = document.getElementById(elementId);
+  element.classList.add('bg-orange-400');
 }
