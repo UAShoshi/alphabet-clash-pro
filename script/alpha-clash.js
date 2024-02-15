1840489f3ef4f18c0c2bd16eec4951c3a8737d9b
UAShoshi <ummeaymonafifha@gmail.com>
2024-02-15 15:27:07 +0600

    start javaScript

---
+++ script/alpha-clash.js
@@ -0,0 +1,10 @@
+function play() {
+  // home-screen Section
+  const homeSection = document.getElementById('home-screen');
+  homeSection.classList.add('hidden');
+  
+  // play-ground Section 
+  const playGroundSection = document.getElementById('play-ground');
+  playGroundSection.classList.remove('hidden');
+  // console.log('play Now pro');
+}
